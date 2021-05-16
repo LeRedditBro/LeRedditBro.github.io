b0fca2eb4f6fcd092fb76243de5479eea21b5ac2
Tomer Atar <kenyweb7@gmail.com>
2021-05-16 23:55:35 +0300

    gh-pages auto upload

--- src/elements/WireFrame.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Theme from '../Theme'
-
-export default function WireFrame({ thickness = '25%', color = Theme.palette.thirdary }) {
-	return (
-		<rect width="100%" height="100%" strokeWidth={thickness} stroke={color} fill="transparent" />
-	)
-}
