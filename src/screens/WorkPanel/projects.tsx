bf9ec0725310707c30ef424909f6cacde521b712
Tomer Atar <kenyweb7@gmail.com>
2022-06-05 09:54:34 +0300

    added waystone to projects

--- src/screens/WorkPanel/projects.tsx
+++ src/screens/WorkPanel/projects.tsx
@@ -1,9 +1,11 @@
 import Github from "mdi-material-ui/Github";
 import Npm from "mdi-material-ui/Npm";
 import GamepadSquare from "mdi-material-ui/GamepadSquare";
+import Alien from "mdi-material-ui/Alien";
 
 
 export default [
+	{ icon: <Alien />, name: 'Waystone', description: 'Alien website world-builing exercise', url: 'https://waystone.netlify.app/' },
 	{ icon: <Github />, name: 'SMAR', description: 'Electron Powered Scriptable Javascript Macros', url: 'https://github.com/LeRedditBro/SMAR' },
 	{ icon: <GamepadSquare />, name: 'Magestro', description: 'Magic Visual Programming Language Proof of Concept', url: 'https://magestro.netlify.app/#/Introduction' },
 	{ icon: <GamepadSquare />, name: 'Consolve', description: 'Puzzle Game played through the browsers devtools', url: 'https://consolve.netlify.app/' },
